7fabf19c168cfd51ccb4c1ff4bb5a4309b0e4867
MrLehaHot <LehaHotPlay@yandex.ru>
2023-02-07 22:39:06 +0500

    Добавленны комментарии

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from settings import *
+from settings import * # Импортируем настройки (Токен и т.д)
 import discord # Подключаем библиотеку
 from discord.ext import commands
 
-intents = discord.Intents.all()
+intents = discord.Intents.all() # Выдаём все права
 
 
 # Задаём префикси интетнты
@@ -14,17 +14,17 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 @bot.event
 
 async def on_member_join(member):
-    channel = bot.get_channel(1072220205469667339)
-    role = member.guild.get_role(1057384839759798342)
-    await member.add_roles(role)
-    await channel.send(embed = discord.Embed(
+    channel = bot.get_channel(1072220205469667339) # Канал "верификация"
+    role1 = member.guild.get_role(1057384839759798342) # Роль "NOT VIRIFIED"
+    await member.add_roles(role1) # При входе даём роль: NOT VIRIFIED
+    await channel.send(embed = discord.Embed( # Отправляем эмбет
         title = 'Новый путник',
         description = f'Путник под никнеймом "{member} - " ступил на наш сервер',
         colour = discord.Colour.from_rgb(106, 192, 245)))
 
 
 @bot.event
-async def on_raw_reaction_add(payload):
+async def on_raw_reaction_add(payload): # Если кто-то поставил реакцию
 
     if payload.message_id == 1057580872997535754: # Если сообщение есть то:
         member = payload.member # Пользыватель
@@ -36,4 +36,4 @@ async def on_raw_reaction_add(payload):
         
 
 
-bot.run(token)
+bot.run(token) # Запускаем бота
